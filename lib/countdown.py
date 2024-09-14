d7f397c1471e9883292af3f639af55fe280c7651
Silvanos Eric <ericoderito@gmail.com>
2024-09-14 12:43:47 +0300

    feat(lib): add countdown function to countdown.py
    Added a countdown function that counts down from a specified limit and prints "HAPPY NEW YEAR!" when
    finished.

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,8 @@
 # your code goes here!
+def countdown(limit: int) -> None:
+    """Counts down from limit and prints "HAPPY NEW YEAR!"."""
+    counter = limit
+    while counter > 0:
+        print(f"{counter} SECOND(S)!")
+        counter -= 1
+    print("HAPPY NEW YEAR!")
